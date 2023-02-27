b3bc4ed745fbf12b584d2fa0cb9efdaa0ec0a0d9
IvyLiu0 <86639583+IvyLiu0@users.noreply.github.com>
2023-02-27 12:14:40 -0800

    added vue data

---
+++ js/data_text.js
@@ -0,0 +1,25 @@
+
+var app = new Vue ({
+    el: '#teachingText',
+    data:{
+        teachingHeading: 'Hands On Teaching',
+        teachingDes: 'Splashes Aquatics Swim School provides high quality swim instruction and the ideal environment for learning how to swim. Our swim instructors use specialized “muscle memory” teaching techniques that help our students progress at a quick rate. Grouped by age and ability, our students benefit from swim classes that offer personalized attention. It\'s never too late to learn to swim!',
+        teachingHead: 'Splashes Offers:',
+        teachingLi1:'Private Swimming Lessons - All Locations',
+        teachingLi2:'Semi-Private Swimming Lessons - All Locations',
+        teachingLi3:'Parent & Me (baby) Swimming Lessons - Select Locations',
+        teachingDes1:'Select locations also offer additional swim programs such as swim competitions, swim teams, water aerobics, aquatic therapy, swim camps and more! To learn more about the Splashes Family Swim School near you, select a location below.',
+        teachingSignup:'Ready to sign up?',
+    }
+});
+
+
+var app = new Vue ({
+    el: '#locationText',
+    data:{
+        locationHeading: 'Find a Splashes Swim School near you',
+        locationHead: 'Family Swim School',
+        locationHead1: 'Locations',
+        locationDes:'Splashes Aquatics offers the finest professional swimming lessons from 21 swim schools and aquatic centers in California and Denver.',
+    }
+});
